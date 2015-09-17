974a8e48edcd2e0e3f1ca7a1e86d3b0e6040a7fc
qubick <qubick.kim@gmail.com>
2015-09-17 14:19:41 -0600

    lab3

---
+++ lab3/lab3/ViewController.swift
@@ -0,0 +1,71 @@
+//
+//  ViewController.swift
+//  lab3
+//
+//  Created by JeeeunKim on 9/17/15.
+//  Copyright (c) 2015 JeeeunKim. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UITextFieldDelegate {
+
+
+    @IBOutlet weak var checkAmout: UITextField!
+    @IBOutlet weak var tipPercent: UITextField!
+    @IBOutlet weak var people: UITextField!
+    
+    @IBOutlet weak var tipDue: UILabel!
+    @IBOutlet weak var totalDue: UILabel!
+    
+    @IBOutlet weak var totalDuePerPerson: UILabel!
+    
+    func updateTipTotals(){
+        
+        let amout       = (checkAmout.text as NSString).floatValue
+        let pct         = (tipPercent.text as NSString).floatValue/100
+        let numOfPeople = people.text.toInt()
+        let tip         = amout * pct
+        let total       = amout + tip
+        var personTotal : Float = 0.0
+        if numOfPeople != nil {
+            if numOfPeople! > 0 {
+                personTotal = total / Float(numOfPeople!)
+            }
+        }
+        
+        var curFormatter        = NSNumberFormatter()
+        //creat curForm obj. from NSFormatter class
+        curFormatter.numberStyle = NSNumberFormatterStyle.CurrencyStyle
+        tipDue.text             = curFormatter.stringFromNumber(tip)
+        totalDue.text           = curFormatter.stringFromNumber(total)
+        totalDuePerPerson.text  = curFormatter.stringFromNumber(personTotal)
+    }
+    
+    func textFieldDidEndEditing(textField: UITextField) {
+        updateTipTotals()
+    }
+    
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+    
+    
+    override func viewDidLoad() {
+        checkAmout.delegate = self
+        tipPercent.delegate = self
+        people.delegate = self
+        
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ lab3/lab3Tests/lab3Tests.swift
@@ -0,0 +1,36 @@
+//
+//  lab3Tests.swift
+//  lab3Tests
+//
+//  Created by JeeeunKim on 9/17/15.
+//  Copyright (c) 2015 JeeeunKim. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class lab3Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
