78c06fa3820dbe53d3c768d73cc75aed6ff954d6
qubick <qubick.kim@gmail.com>
2015-09-27 17:38:33 -0600

    view layout

---
+++ project1/Project1/Project1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Project1
+//
+//  Created by JeeeunKim on 9/27/15.
+//  Copyright (c) 2015 JeeeunKim. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
