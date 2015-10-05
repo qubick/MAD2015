2bd6ac98548ea77484072d3f334b82797217284b
qubick <qubick.kim@gmail.com>
2015-10-05 16:01:31 -0600

    initial layout and custom button supporting files created

---
+++ project2/project2/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  project2
+//
+//  Created by JeeeunKim on 10/5/15.
+//  Copyright (c) 2015 JeeeunKim. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var btnWhite1: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
