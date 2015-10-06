70e7c0c528d8c9f1848a66231926ec0adc723b73
qubick <qubick.kim@gmail.com>
2015-10-06 10:20:26 -0600

    animated images

--- lab4/lab4/ViewController.swift
+++ lab4/lab4/ViewController.swift
@@ -10,16 +10,44 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var delta = CGPointMake(12, 4)
+    var nemoRadius = CGFloat()
+    var timer = NSTimer()
+    var translation = CGPointMake(0.0, 0.0)
+    var angle:CGFloat = 0.0
     
     @IBOutlet weak var sliderLabel: UILabel!
     @IBOutlet weak var imgNemo: UIImageView!
     @IBOutlet weak var sliderSpeed: UISlider!
     @IBAction func sliderMoved(sender: UISlider) {
+        timer.invalidate()
+        changeSliderValue()
+    }
+    
+    func changeSliderValue(){
+        sliderLabel.text = String(format: "%.2f", sliderSpeed.value)
+        timer = NSTimer.scheduledTimerWithTimeInterval(Double(sliderSpeed.value), target:self, selector:"moveImg", userInfo:nil, repeats: true)
+    }
+    
+    func moveImg(){
+        
+        imgNemo.center = CGPointMake(imgNemo.center.x + delta.x, imgNemo.center.y + delta.y)
+        
+        if imgNemo.center.x > view.bounds.size.width - nemoRadius || imgNemo.center.x < nemoRadius {
+            delta.x = -delta.x
+        }
+        
+        if imgNemo.center.y > view.bounds.size.height - nemoRadius || imgNemo.center.y < nemoRadius {
+            delta.y = -delta.y
+        }
     }
     
     override func viewDidLoad() {
+        
+        nemoRadius = imgNemo.frame.size.width/2
+        changeSliderValue()
+        
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
 
     override func didReceiveMemoryWarning() {
