2cd8cc96cd2ec186d25e3ed4cb0679b0a1657227
Jeeeun Kim <jeki4699@atlas113-20.colorado.edu>
2015-10-29 09:18:17 -0600

    initial commit

---
+++ midterm/midterm/ViewController.swift
@@ -0,0 +1,42 @@
+//
+//  ViewController.swift
+//  midterm
+//
+//  Created by Jeeeun Kim on 10/29/15.
+//  Copyright (c) 2015 Jeeeun Kim. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var currentGallons: UILabel!
+    @IBOutlet weak var totaltime: UILabel!
+    @IBOutlet weak var totalGallons: UILabel!
+    
+    
+    @IBAction func typeMiles(sender: UITextField) {
+    }
+    @IBAction func onOffMonthly(sender: UISwitch) {
+    }
+    @IBAction func slideGallons(sender: UISlider) {
+        let oil = sender.value
+        currentGallons.text = String(format: ".0f", oil)
+    }
+    
+    @IBAction func chooseRide(sender: AnyObject) {
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
