f304693595445fd8bada061749fe09500ef2dd0f
Jeeeun Kim <jeki4699@atlas113-20.colorado.edu>
2015-10-29 09:50:14 -0600

    calculation done, textfield delegated

--- midterm/midterm/ViewController.swift
+++ midterm/midterm/ViewController.swift
@@ -8,16 +8,17 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
 
+    @IBOutlet weak var dailyMiles: UITextField!
     @IBOutlet weak var currentGallons: UILabel!
+    @IBOutlet weak var slider: UISlider!
+    @IBOutlet weak var segmentRide: UISegmentedControl!
     @IBOutlet weak var totaltime: UILabel!
     @IBOutlet weak var totalGallons: UILabel!
     @IBOutlet weak var imgRide: UIImageView!
     
     
-    @IBAction func typeMiles(sender: UITextField) {
-    }
     @IBAction func onOffMonthly(sender: UISwitch) {
     }
     @IBAction func slideGallons(sender: UISlider) {
@@ -39,7 +40,43 @@ class ViewController: UIViewController {
         }
     }
     
+    func calculate() {
+    
+        var totalMile:Float?
+        let curFormatter = NSNumberFormatter()
+        let miles = (dailyMiles.text as NSString).floatValue
+        
+        switch segmentRide.selectedSegmentIndex {
+        case 0: //car
+             totalMile = miles/20*60 //when average speed is 20mph
+        case 1: //bus
+            totalMile = miles/12*60 + 5
+        case 2: //bike
+            totalMile = miles/10
+        default:
+            println("this should not happens")
+        }
+        
+        let gallons = (dailyMiles.text as NSString).floatValue
+        let totalGallon = gallons/24 //when average efficiency is 24 mpg
+        
+        //curFormatter.numberStyle = NSNumberFormatterStyle.number
+        totaltime.text = curFormatter.stringFromNumber(totalMile!)
+        totalGallons.text = curFormatter.stringFromNumber(totalGallon)
+    }
+    
+    func textFieldDidEndEditing(textField: UITextField) {
+        calculate()
+    }
+    
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+    
     override func viewDidLoad() {
+        dailyMiles.delegate = self
+        
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
