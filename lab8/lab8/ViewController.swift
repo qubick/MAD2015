df7dfa9849a1639d0237d6b964403faf15e52127
qubick <qubick.kim@gmail.com>
2015-10-28 20:29:10 -0600

    data sent, app icons/launch screen added

--- lab8/lab8/ViewController.swift
+++ lab8/lab8/ViewController.swift
@@ -13,13 +13,41 @@ class ViewController: UIViewController {
     @IBOutlet weak var titleLabel: UILabel!
     @IBOutlet weak var artistLabel: UILabel!
     @IBOutlet weak var imgArt: UIImageView!
+    @IBOutlet weak var answerCorrect: UILabel!
     
     var user = Favorite()
-    
+    var correct:Bool = false
+    var answerAList = ["Starry Starry Night", "Starry Starry Night ", "starry starry night", "starry starry night ", "Starry starry night", "Starry starry night ", "Starry starry night", "Starry starry night "]
+    var answerBList = ["Vincent", "vincent", "Vincent Van Gogh", "Vincent van Gogh", "Vincent van gogh", "Van Gogh", "Van gogh", "Gogh", "goh", "Vincent ", "vincent ", "Vincent Van Gogh ", "Vincent van Gogh ", "Vincent van gogh ", "Van Gogh ", "Van gogh ", "Gogh ", "goh "]
     
     @IBAction func unwindSegue(segue: UIStoryboardSegue){
+        
+        let answerA = user.favArt
+        let answerB = user.favArtist
+        
+        for i in 0...3 {
+            if answerA == answerAList[i] {
+                for j in 0...7 {
+                    if answerB == answerBList[j] {
+                        correct = true
+                        break
+                    }
+                }
+            }
+        }
+        
         titleLabel.text = user.favArt
         artistLabel.text = user.favArtist
+        
+        if correct == true {
+            answerCorrect.textColor = UIColor.greenColor()
+            answerCorrect.text = "Correct!"
+            correct = false
+        } else {
+            answerCorrect.textColor = UIColor.redColor()
+            answerCorrect.text = "Try Again"
+            correct = false
+        }
     }
     
     override func viewDidLoad() {
