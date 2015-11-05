fc87bf7f33749bbd1a901a4352c6d6f513d356bf
qubick <qubick.kim@gmail.com>
2015-11-05 10:37:22 -0700

    textview conencted resource

--- lab9/app/src/main/java/com/example/jeeeunkim/lab9/MainActivity.java
+++ lab9/app/src/main/java/com/example/jeeeunkim/lab9/MainActivity.java
@@ -2,8 +2,16 @@ package com.example.jeeeunkim.lab9;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity { //controller view in iOS
+    public void findMood(View view){
+        TextView feeling = (TextView)findViewById(R.id.feelingText);
+        feeling.setText("I'm in the mood to create Android apps!");
+
+
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
