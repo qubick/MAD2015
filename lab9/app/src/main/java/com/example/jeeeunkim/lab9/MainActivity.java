ae7263c058b9521ff7b1884f9334bd2d238cce80
qubick <qubick.kim@gmail.com>
2015-11-05 10:51:01 -0700

    got selected value of dropdown button

--- lab9/app/src/main/java/com/example/jeeeunkim/lab9/MainActivity.java
+++ lab9/app/src/main/java/com/example/jeeeunkim/lab9/MainActivity.java
@@ -3,14 +3,21 @@ package com.example.jeeeunkim.lab9;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Spinner;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity { //controller view in iOS
     public void findMood(View view){
         TextView feeling = (TextView)findViewById(R.id.feelingText);
-        feeling.setText("I'm in the mood to create Android apps!");
+        Spinner moodSpinner = (Spinner)findViewById(R.id.spinner);
+        String moodText = String.valueOf(moodSpinner.getSelectedItem());
+        feeling.setText("I'm in a " + moodText + " mood!");
 
-
+        /*
+        if (moodText == "happy"){
+            feeling.setText("I am happy too!");
+        }
+        */
     }
 
     @Override
